832e8af3b676672ced9d843c89d53c5279e823e3
haidi nurhadinata <nhaidii@yahoo.com>
2018-09-14 18:52:09 +0800

    update table customer

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -5,7 +5,7 @@
       <div class="col-sm-4">
           <div class="page-header float-left">
               <div class="page-title">
-                  <h1>Dashboard</h1>
+                  <h1>Customer</h1>
               </div>
           </div>
       </div>
@@ -23,9 +23,49 @@
   </div>
 
   <div class="content mt-3">
-    <div class="col-md-12">
-      <h1>Customer</h1>
-    </div>
-  </div>
+      <div class="animated fadeIn">
+          <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-header">
+                        <strong class="card-title">Stripped Table</strong>
+                    </div>
+                    <div class="card-body">
+                        <table class="table table-striped">
+                          <thead>
+                            <tr>
+                              <th scope="col">#</th>
+                              <th scope="col">First</th>
+                              <th scope="col">Last</th>
+                              <th scope="col">Handle</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <th scope="row">1</th>
+                              <td>Mark</td>
+                              <td>Otto</td>
+                              <td>@mdo</td>
+                            </tr>
+                            <tr>
+                              <th scope="row">2</th>
+                              <td>Jacob</td>
+                              <td>Thornton</td>
+                              <td>@fat</td>
+                            </tr>
+                            <tr>
+                              <th scope="row">3</th>
+                              <td>Larry</td>
+                              <td>the Bird</td>
+                              <td>@twitter</td>
+                            </tr>
+                          </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+          </div>
+      </div><!-- .animated -->
+  </div><!-- .content -->
 
 @endsection
